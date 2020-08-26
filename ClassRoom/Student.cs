304af55c80dbbcf13be37b8436712fba5364dde9
Alex F <vlad.faget@outlook.com>
2020-08-26 12:58:51 +0200

    Task 1
    Completed

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        private string Name;
+        private int BirthMonth;
+        private int BirthDay;
+
+        protected void SetName(string NewName)
+        {
+            Name = NewName;
+        }
+
+        protected string GetName()
+        {
+            return Name;
+        }
+
+        protected void SetBirthDay(int Day)
+        {
+            //should verify the actual date exists
+            BirthDay = Day;
+        }
+
+        protected int GetBirthDay()
+        {
+            return BirthDay;
+        }
+
+        protected void SetBirthMonth(int Month)
+        {
+            BirthMonth = Month;
+        }
+
+        protected int GetBirthMonth()
+        {
+            return BirthMonth;
+        }
+
+    }
+}
